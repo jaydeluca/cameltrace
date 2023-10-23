67ceee6de661dbf9aa54b773f7b89efd27dff8da
Jay DeLuca <jaydeluca4@gmail.com>
2023-10-23 08:06:15 -0400

    add in datadog (#2)

--- runtest.sh
+++ runtest.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
-docker compose down
-docker compose up -d
+#docker compose down
+#docker compose up -d
 
-./gradlew clean build -x test
+#./gradlew clean build -x test
+#
+#java -javaagent:opentelemetry-javaagent.jar \
+#     -Dotel.resource.attributes=service.name=test-service \
+#     -Dotel.traces.exporter=otlp \
+#     -Dotel.exporter.otlp.endpoint=http://127.0.0.1:4317 \
+#     -jar build/libs/cameltrace-0.0.1-SNAPSHOT.jar
 
-java -javaagent:opentelemetry-javaagent.jar \
-     -Dotel.resource.attributes=service.name=test-service \
-     -Dotel.traces.exporter=otlp \
-     -Dotel.exporter.otlp.endpoint=http://127.0.0.1:4317 \
+java -javaagent:dd-java-agent-1.20.0.jar \
+     -Ddd.service=camel-trace \
+     -Ddd.env=dev \
+     -Ddd.trace.debug=true \
+     -Ddd.trace.otel.enabled=true \
+     -Ddd.integration.opentelemetry.experimental.enabled=true \
+     -Ddd.otel.exporter.otlp.endpoint=localhost:4317 \
      -jar build/libs/cameltrace-0.0.1-SNAPSHOT.jar
